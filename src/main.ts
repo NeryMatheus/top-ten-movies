deaa77fd05a3d9a983d0a49cc34b943042fb8881
Matheus Nery <matheusnery345@gmail.com>
2023-06-13 21:34:31 -0400

    [fix] Atualização do CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: ["GET", "POST"],
+    credentials: true,
+  });// Habilita o CORS
+
   await app.listen(3000);
 }
 bootstrap();
